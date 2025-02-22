acba09e545ceb6ddd627e6e46645cdb7d1b002c2
Nicholas Verrochi <verrochi92@gmail.com>
2025-02-22 17:08:23 -0500

    small changes

---
+++ RPGEngine/src/model/GameFile.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * GameFile.java
+ * Represents the game data file
+ * By Nicholas Verrochi
+ */
+
+public class GameFile {
+
+    public GameFile() {
+
+    }
+
+    public GameFile(String path) {
+
+    }
+
+}
--- RPGEngine/src/view/MainMenu.java
+++ RPGEngine/src/view/MainMenu.java
@@ -7,13 +7,30 @@ package view;
  */
 
 public class MainMenu extends Menu {
+
+    public MainMenu() {
+
+    }
+
     @Override
     public void display() {
-        System.out.println("Welcome to the game!");
+        System.out.println("Welcome to RPGMaker!");
+        System.out.println("Copyright 2024 Nicholas Verrochi\n");
+
+        printGameStatus();
+
+        System.out.println("Please make a selection:");
+        System.out.println("1. Load game file");
+        System.out.println("2. Create new file");
+        System.out.println("3. Export current file");
     }
 
     @Override
     public boolean makeChoice(int i) {
         return false;
+    }
+
+    public void printGameStatus() {
+
     }
 }
